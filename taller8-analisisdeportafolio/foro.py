eab59693afb4d6e949f68a37287e9b5d5ea7886b
VILLA ROJAS SAMUEL <savillaro@uide.edu.ec>
2026-07-21 11:54:49 -0500

    taller8: la primera entidad del proyecto

---
+++ taller8-analisisdeportafolio/foro.py
@@ -0,0 +1,17 @@
+class foro:
+    def __init__ (self, 
+                  id_foro = 0, 
+                  titulo = "", 
+                  descripcion= "", 
+                  fecha_creacion = "", 
+                  fecha_inicio = "", 
+                  fecha_cierre = "", 
+                  estado = True, 
+                  tema_principal = "", 
+                  cantidad_mensajes = 0, 
+                  cantidad_participantes = 0,
+                  id_curso = 0, 
+                  id_docente = 0
+                  ):
+        print("Constuctor con argumentos")
+    
---
+++ taller8-analisisdeportafolio/main.py
@@ -0,0 +1,3 @@
+from foro import foro
+
+foro_1 = foro(1, "el inicio del proyecto", "este proyecto nos va a causar mucho estres, pero al final va a ser util", "21/07/26", "21/07/26", "21/07/26", True, "El proyecto", 1, 1, 1, 1)
